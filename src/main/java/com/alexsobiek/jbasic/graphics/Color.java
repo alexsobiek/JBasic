aa82981f32338b2fdf5653b6d20495566ead40a9
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-10-22 21:00:13 -0500

    Cleaned up

--- src/main/java/com/alexsobiek/jbasic/graphics/Color.java
+++ src/main/java/com/alexsobiek/jbasic/graphics/Color.java
@@ -8,16 +8,10 @@ package com.alexsobiek.jbasic.graphics;
  *
  * Bit    7  6  5  4  3  2  1  0
  * Data   R  R  R  G  G  G  B  B
+ *
+ * TODO: White (255) does not translate to a perfect white, but instead to r=255, g=224, b=192
  */
 public class Color {
-    public static void main(String[] args) {
-        byte a = toByte(0xb2, 0x66, 0xFF); // 178, 102, 255
-        System.out.println(from(a)); // 160, 96, 192
-
-        byte b = toByte(0x97, 0xC0, 0x60); // 151, 102, 96
-        System.out.println(from(b)); // 255, 192, 64
-    }
-
     /**
      * Returns 8-bit color byte
      * @param r Red value
@@ -25,8 +19,17 @@ public class Color {
      * @param g Green value
      * @return byte
      */
-    public static byte toByte(int r, int g, int b) {
+    public byte toByte(int r, int g, int b) {
         return (byte) (((r >> 5) << 5) | ((g >> 5) << 2) | (b >> 6));
+    }
+
+    /**
+     * Returns 8-bit color byte from Java Color object
+     * @param color Java Color
+     * @return byte
+     */
+    public byte fromJColor(java.awt.Color color) {
+        return toByte(color.getRed(), color.getGreen(), color.getBlue());
     }
 
 
@@ -35,7 +38,7 @@ public class Color {
      * @param rgb 8-bit color
      * @return java.awt.Color
      */
-    public static java.awt.Color from(int rgb) {
+    public java.awt.Color from(int rgb) {
         rgb = rgb << 24;
         int r = (rgb >> 29) << 29;
         int g = (((rgb << 3) >> 29) << 21);
