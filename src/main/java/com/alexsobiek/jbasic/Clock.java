9d0fceab8bc4ef8beb68df16870ba5afa0dacd9a
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-10-22 21:55:12 -0500

    Added Clock

---
+++ src/main/java/com/alexsobiek/jbasic/Clock.java
@@ -0,0 +1,16 @@
+package com.alexsobiek.jbasic;
+
+import com.alexsobiek.jbasic.event.EventBus;
+import com.alexsobiek.jbasic.event.events.CPUCycle;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Clock {
+    public Clock(EventBus eventBus) {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        CPUCycle cycle = new CPUCycle();
+        service.scheduleAtFixedRate(() -> eventBus.post(cycle), 0, 1000, TimeUnit.MILLISECONDS);
+    }
+}
