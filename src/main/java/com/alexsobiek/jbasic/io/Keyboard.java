0725d40fd08ace34e41d8de7c4e6fd6dc5b7f8e7
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-11-04 21:49:15 -0500

    Added key input buffering

--- src/main/java/com/alexsobiek/jbasic/io/Keyboard.java
+++ src/main/java/com/alexsobiek/jbasic/io/Keyboard.java
@@ -1,23 +1,105 @@
 package com.alexsobiek.jbasic.io;
 
+import com.alexsobiek.jbasic.Program;
 import com.alexsobiek.jbasic.event.EventBus;
+import com.alexsobiek.jbasic.event.EventListener;
+import com.alexsobiek.jbasic.event.Listener;
 import com.alexsobiek.jbasic.event.events.KeyInputEvent;
+import com.alexsobiek.jbasic.event.events.ProcessorCycle;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
-public class Keyboard {
+public class Keyboard implements Listener {
+    private final KeyBuffer buffer;
+    private final EventBus eventBus;
     public Keyboard(EventBus eventBus) {
+        this.buffer = new KeyBuffer();
+        this.eventBus = eventBus;
+        eventBus.subscribe(this);
+
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(event -> {
             switch (event.getID()) {
                 case KeyEvent.KEY_PRESSED:
-                    eventBus.post(new KeyInputEvent.Pressed(event.getKeyChar(), event.getKeyCode()));
+                    buffer.push(new KeyInputEvent.Pressed(event.getKeyChar(), event.getKeyCode()));
                     break;
                 case KeyEvent.KEY_RELEASED:
-                    eventBus.post(new KeyInputEvent.Released(event.getKeyChar(), event.getKeyCode()));
+                    // buffer.push(new KeyInputEvent.Released(event.getKeyChar(), event.getKeyCode()));
                     break;
             }
             return true;
         });
     }
+
+    @EventListener
+    public void onProcessorCycle(ProcessorCycle event) {
+        if (buffer.peek() != null) {
+            eventBus.post(buffer.pop());
+        }
+    }
+
 }
+class KeyBuffer {
+    private Node head;
+    private Node current;
+
+    public enum ACTION { CursorMove }
+
+    public KeyBuffer() { }
+
+    public void push(KeyInputEvent key) {
+        Node n = new Node(key);
+        if (head == null) {
+            head = n;
+            current = head;
+        }
+        else {
+            current.setNext(n);
+            current = n;
+        }
+        if (head == null) head = n;
+    }
+
+
+
+    public KeyInputEvent pop() {
+        KeyInputEvent removed = head.getKey();
+        if (head.hasNext()) head = head.getNext();
+        else head = null;
+        return removed;
+    }
+
+    public KeyInputEvent peek() {
+        return (head != null) ? head.getKey() : null;
+    }
+
+    class Node {
+        private KeyInputEvent key;
+        private Node next;
+
+        protected Node(KeyInputEvent key) {
+            this.key = key;
+        }
+
+        protected KeyInputEvent getKey() {
+            return key;
+        }
+
+        protected void setKey(KeyInputEvent key) {
+            this.key = key;
+        }
+
+        protected boolean hasNext() {
+            return next != null;
+        }
+
+        protected Node getNext() {
+            return next;
+        }
+
+        protected void setNext(Node next) {
+            this.next = next;
+        }
+    }
+}
+
